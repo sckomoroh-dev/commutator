b3873a46e6c1bb4f00a411bcfe4161b9f9230922
Yehor Zvihunov <sckomoroh@gmail.com>
2018-05-05 11:03:34 +0300

    server

--- server/udp/UdpServer.h
+++ server/udp/UdpServer.h
@@ -13,80 +13,86 @@
 
 using UdpClientSocketPtr = std::unique_ptr<network::sockets::udp::UdpClientSocket>;
 
-class UdpServer
+namespace server
 {
-private:
-    network::sockets::udp::UdpServerSocket _udpServerSocket;
-    std::atomic<bool> _needStopServer;
-
-public:
-    template<typename TServerIp, typename TPort>
-    UdpServer(TServerIp&& serverIp, TPort&& port)
-        : _udpServerSocket(serverIp, port)
-        , _needStopServer(false)
+    namespace udp
     {
-    }
-
-    virtual ~UdpServer()
-    {
-
-    }
-
-    void startServer()
-    {
-        _udpServerSocket.bind();
-    }
-
-    void waitIncommingRequests()
-    {
-        while (_needStopServer == false)
+        class UdpServer
         {
-            uint32_t messageHeaderLen;
-            auto clientSocket = _udpServerSocket.readBuffer(static_cast<void *>(&messageHeaderLen),
-                                                            sizeof(messageHeaderLen));
-            if (messageHeaderLen < 1)
+        private:
+            network::sockets::udp::UdpServerSocket _udpServerSocket;
+            std::atomic<bool> _needStopServer;
+
+        public:
+            template<typename TServerIp, typename TPort>
+            UdpServer(TServerIp &&serverIp, TPort &&port)
+                    : _udpServerSocket(serverIp, port)
+                    , _needStopServer(false)
             {
-                continue;
             }
 
-            std::thread clientThread(clientMethod, this, std::move(clientSocket), messageHeaderLen);
-        }
-    }
-
-private:
-    static void clientMethod(UdpServer* thisPtr, UdpClientSocketPtr&& clientSocket, uint32_t firstMessageLen)
-    {
-        uint32_t messageLen = firstMessageLen;
-
-        while (true)
-        {
-            thisPtr->readClientMessage(std::move(clientSocket), messageLen);
-
-            messageLen = thisPtr->readClientMessageLength(std::move(clientSocket));
-            if (messageLen == 0)
+            virtual ~UdpServer()
             {
-                break;
+
             }
-        }
+
+            void startServer()
+            {
+                _udpServerSocket.bind();
+            }
+
+            void waitIncommingRequests()
+            {
+                while (_needStopServer == false)
+                {
+                    uint32_t messageHeaderLen;
+                    auto clientSocket = _udpServerSocket.readBuffer(static_cast<void *>(&messageHeaderLen),
+                                                                    sizeof(messageHeaderLen));
+                    if (messageHeaderLen < 1)
+                    {
+                        continue;
+                    }
+
+                    std::thread clientThread(clientMethod, this, std::move(clientSocket), messageHeaderLen);
+                }
+            }
+
+        private:
+            static void clientMethod(UdpServer *thisPtr, UdpClientSocketPtr &&clientSocket, uint32_t firstMessageLen)
+            {
+                uint32_t messageLen = firstMessageLen;
+
+                while (true)
+                {
+                    thisPtr->readClientMessage(std::move(clientSocket), messageLen);
+
+                    messageLen = thisPtr->readClientMessageLength(std::move(clientSocket));
+                    if (messageLen == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            uint32_t readClientMessageLength(UdpClientSocketPtr &&clientSocket)
+            {
+                uint32_t messageHeaderLen;
+
+                clientSocket->readBuffer(static_cast<void *>(&messageHeaderLen), sizeof(messageHeaderLen));
+
+                return messageHeaderLen;
+            }
+
+            void readClientMessage(UdpClientSocketPtr &&clientSocket, uint32_t messageLen)
+            {
+                char *buffer = static_cast<char *>(malloc(messageLen + 1));
+                clientSocket->readBuffer(static_cast<void *>(buffer), messageLen);
+
+                // TODO: do somtethid with buffer like respond
+                printf("[SERVER-client] Received:\n----------------------\n\n%s\n--------------------\n\n", buffer);
+            }
+        };
     }
-
-    uint32_t readClientMessageLength(UdpClientSocketPtr&& clientSocket)
-    {
-        uint32_t messageHeaderLen;
-
-        clientSocket->readBuffer(static_cast<void *>(&messageHeaderLen), sizeof(messageHeaderLen));
-
-        return messageHeaderLen;
-    }
-
-    void readClientMessage(UdpClientSocketPtr&& clientSocket, uint32_t messageLen)
-    {
-        char* buffer = static_cast<char*>(malloc(messageLen + 1));
-        clientSocket->readBuffer(static_cast<void*>(buffer), messageLen);
-
-        // TODO: do somtethid with buffer like respond
-        printf("[SERVER-client] Received: %s\n--------------------\n\n", buffer);
-    }
-};
+}
 
 #endif //COMMUTATOR_UDPSERVER_H
